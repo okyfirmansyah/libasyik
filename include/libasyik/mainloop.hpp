f0963d79efc90ca880e52d8e08c99a1ec531dd23
Oky Firmansyah <oky.firmansyah@nodeflux.io>
2020-05-25 08:13:00 +0700

    initial commit

---
+++ include/libasyik/mainloop.hpp
@@ -0,0 +1,11 @@
+#ifndef VSCODE_TEMPLATE_MAINLOOP_HPP
+#define VSCODE_TEMPLATE_MAINLOOP_HPP
+#include <string>
+
+namespace asyik {
+
+void fn(std::string const& str, int n);
+
+}
+
+#endif
---
+++ src/mainloop.cpp
@@ -0,0 +1,15 @@
+#include <boost/fiber/all.hpp>
+#include <iostream>
+#include <libasyik/mainloop.hpp>
+
+namespace asyik {
+
+void fn(std::string const& str, int n)
+{
+  for (int i = 0; i < n; ++i) {
+    std::cout << i << ": " << str << std::endl;
+    boost::this_fiber::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+}  // namespace asyik
