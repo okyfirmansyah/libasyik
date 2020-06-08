90b564c244beddaa99c478a70e8879ba4fb22c87
Oky Firmansyah <oky.firmansyah@nodeflux.io>
2020-06-08 09:42:41 +0700

    add async functionalities; move test to individual cpp files

--- include/libasyik/service.hpp
+++ include/libasyik/service.hpp
@@ -1,6 +1,7 @@
 #ifndef LIBASYIK_ASYIK_SERVICE_HPP
 #define LIBASYIK_ASYIK_SERVICE_HPP
 #include <string>
+#include <type_traits>
 #include "boost/fiber/all.hpp"
 #include "libasyik/common.hpp"
 #include "aixlog.hpp"
@@ -12,6 +13,32 @@ namespace asio = boost::asio;
 
 namespace asyik
 {
+  void _TEST_invoke_service();
+
+  namespace service_internal
+  {
+    template <typename R>
+    struct helper
+    {
+      template <typename P, typename F, typename... Args>
+      static void set(P &&p, F &&f, Args &&... args)
+      {
+        p->set_value(f(std::forward<Args>(args)...));
+      }
+    };
+
+    template <>
+    struct helper<void>
+    {
+      template <typename P, typename F, typename... Args>
+      static void set(P &&p, F &&f, Args &&... args)
+      {
+        f(std::forward<Args>(args)...);
+        p->set_value();
+      }
+    };
+  }; // namespace service_internal
+
   class service;
   using service_ptr = std::shared_ptr<service>;
   using service_wptr = std::weak_ptr<service>;
@@ -35,26 +62,68 @@ namespace asyik
     template <typename T, typename... V>
     void execute(T &&t, V &&... v)
     {
-      boost::fibers::fiber fb([t2 = std::forward<T>(t)](V &&... v) { t2(v...); });
+      fiber fb([t2 = std::forward<T>(t)](V &&... v) { t2(v...); });
       fb.detach();
     }
 
     template <typename T, typename... V>
     fiber spawn(T &&t, V &&... v)
     {
-      boost::fibers::fiber fb([t2 = std::forward<T>(t)](V &&... v) { t2(v...); });
+      fiber fb([t2 = std::forward<T>(t)](V &&... v) { t2(v...); });
       return std::move(fb);
     }
 
+    template <typename F, typename... Args>
+    fibers::future<typename std::result_of<F(Args...)>::type> async(F &&fun, Args &&... args)
+    {
+      if (!workers_initiated)
+        init_workers();
+
+      fibers::promise<typename std::result_of<F(Args...)>::type> promise;
+      auto future = promise.get_future();
+      auto p = std::make_shared<fibers::promise<typename std::result_of<F(Args...)>::type>>(std::move(promise));
+      tasks->push([f = std::forward<F>(fun),
+                   &args...,
+                   p]() mutable {
+        try
+        {
+          service_internal::helper<typename std::result_of<F(Args...)>::type>::set(p, f, std::forward<Args>(args)...);
+        }
+        catch (...)
+        {
+          p->set_exception(std::current_exception());
+        };
+      });
+
+      return std::move(future);
+    };
+
     void run();
-    void stop() { stopped = true; };
+    void stop()
+    {
+      if (workers_initiated)
+      {
+        tasks = nullptr; // will close channel
+        std::for_each(workers.begin(), workers.end(), [](auto &t) {
+          t.join();
+        });
+      };
+      workers_initiated = false;
+      stopped = true;
+    };
 
     boost::asio::io_context &get_io_service() { return io_service; };
+    static void terminate();
 
   private:
     bool stopped;
     boost::asio::io_context io_service;
+    void init_workers();
+    bool workers_initiated;
+    std::vector<std::thread> workers;
+    std::shared_ptr<fibers::buffered_channel<std::function<void()>>> tasks;
 
+  public:
     friend service_ptr make_service();
   };
 
--- src/service.cpp
+++ src/service.cpp
@@ -14,11 +14,15 @@ namespace beast = boost::beast;
 
 namespace asyik
 {
+  void _TEST_invoke_service(){};
+
   service::service(struct service::private_ &&) : stopped(false), io_service()
   {
     AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace,
                                         AixLog::Type::normal);
     fibers::use_scheduling_algorithm<fibers::algo::round_robin>();
+
+    workers_initiated = false;
   }
 
   service_ptr make_service() { return std::make_shared<service>(service::private_{}); }
@@ -49,6 +53,37 @@ namespace asyik
     }
   }
 
+  void service::init_workers()
+  {
+    int pool_size = std::thread::hardware_concurrency();
+    tasks = std::make_shared<fibers::buffered_channel<std::function<void()>>>(1024);
+    workers_initiated = true;
+
+    for (std::size_t i = 0; i < (size_t)pool_size; ++i)
+    {
+      workers.emplace_back([p = shared_from_this()]() {
+        std::function<void()> tsk;
+        while (boost::fibers::channel_op_status::closed != p->tasks->pop(tsk))
+        {
+          tsk();
+        };
+      });
+    };
+  }
+
+  TEST_CASE("very basic fiber execution", "[service]")
+  {
+    auto as = asyik::make_service();
+
+    as->execute([=] {
+      LOG(INFO) << "brief fiber";
+      asyik::sleep_for(std::chrono::milliseconds(1));
+      as->stop();
+    });
+    as->run();
+    REQUIRE(true);
+  }
+
   TEST_CASE("Check spawn() and execute()", "[service]")
   {
     auto as = asyik::make_service();
@@ -69,4 +104,53 @@ namespace asyik
     fb.join();
     REQUIRE(!sequence.compare("AABAB"));
   }
+
+  TEST_CASE("execute async", "[service]")
+  {
+    auto as = asyik::make_service();
+    std::atomic<int> i(0);
+
+    as->async([&i](int inp) {
+        usleep(100);
+        i = inp;
+      },
+              10)
+        .get();
+    as->async([](auto &i) -> void {
+      usleep(0);
+      i++;
+    },
+              i);
+    as->async([&i](string_view s, const std::string s2) -> void {
+        usleep(130);
+        int k = i + s.length() + s2.length();
+        i = k;
+      },
+              "lol", "ok")
+        .get();
+
+    REQUIRE(i == 16);
+
+    as->stop();
+    as->run();
+  }
+
+  TEST_CASE("execute async with return value", "[service]")
+  {
+    auto as = asyik::make_service();
+
+    std::string s =
+        as->async([](int inp) {
+            usleep(100);
+            return std::to_string(inp);
+          },
+                  10)
+            .get();
+
+    REQUIRE(!s.compare("10"));
+
+    as->stop();
+    as->run();
+  }
+
 } // namespace asyik
